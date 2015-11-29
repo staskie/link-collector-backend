97cde7ad6ae5633402b02e9192585d818b0e4c83
Dominik Staskiewicz <stadominik@gmail.com>
2015-11-29 10:16:12 +0100

    Tidy up specs

--- spec/queries/user_categories_query_spec.rb
+++ spec/queries/user_categories_query_spec.rb
@@ -1,11 +1,13 @@
 require 'rails_helper'
 
 describe UserCategoriesQuery do
-  let!(:user)           { create :user }
-  let!(:programming)    { create :category, name: 'Programming' }
-  let!(:marketing)      { create :category, name: 'Marketing' }
-  let!(:link_one)       { create :link, category: programming, user: user }
-  let!(:link_two)       { create :link, category: programming, user: user }
+  let!(:user)        { create :user }
+
+  let!(:programming) { create :category, name: 'Programming' }
+  let!(:marketing)   { create :category, name: 'Marketing' }
+
+  let!(:link_one)    { create :link, category: programming, user: user }
+  let!(:link_two)    { create :link, category: programming, user: user }
 
   describe '.call' do
     subject { described_class.call(user) }
